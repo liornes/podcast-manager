7d916d71eb0767e6456e8556b6f66b445c9e7d2b
Leandro Lima <89042570+liornes@users.noreply.github.com>
2025-08-01 16:48:13 -0300

    ajuste response status code

--- src/services/filter-episodes-service.ts
+++ src/services/filter-episodes-service.ts
@@ -1,10 +1,25 @@
 import { IncomingMessage } from "http";
-import { repositoryPodcast } from "../repositories/podcasts-repository"
+import { repositoryPodcast } from "../repositories/podcasts-repository";
+import { FilterPodcastModel } from "../models/filter-podcast-model";
+import { StatusCode } from "../utils/status-code";
 
 export const serviceFilterEpisodes = async (podcastName: string | undefined) => {
+    let responseFormat: FilterPodcastModel = {
+        statusCode: 0,
+        body: []
+    }
+
     const queryString = podcastName?.split("?p=")[1] ?? "";
 
     const data = await repositoryPodcast(queryString);
+
+    if(data) {
+        responseFormat.statusCode = StatusCode.OK;
+    } else {
+        responseFormat.statusCode = StatusCode.NoContent
+    }
+
+    responseFormat.body = data
 
     return data;
 }
